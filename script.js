9fb99a2cd27b3a30dca83baf417457658b644de5
Alvarenga7 <kiteyyhastenreiter@gmail.com>
2025-06-29 14:22:08 -0300

    add animation to the button

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle('light')
 
-  const img = document.querySelector("#profile img")
 
-  if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-2.jpg')
-
-  } else {
-    img.setAttribute('src', './assets/avatar-light.jpeg')
-  }
 
 }
